abd2a682efe252905aab575cf2c7e8a42d2e5f96
Jeremy Seckinger <jtseck@aol.com>
2021-04-25 10:52:07 -0700

    Updated string solution #4 with comments

--- js/sort-and-star-solutions.js
+++ js/sort-and-star-solutions.js
@@ -1,3 +1,6 @@
 function twoSort(s) {
+    //sorts array of strings by ASCII value, then takes string at [0], splits it, and joins its letters with '***' in the middle
     return s.sort().slice(0,1).toString().split('').join('***');
 }
+
+//A more concise solution would have been to to identify the first sorted value by its index[0], instead of using .slice and .toString
