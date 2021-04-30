9cb27834241cc27ad6282d1d8329f7b9173b352f
Jeremy Seckinger <jtseck@aol.com>
2021-04-30 11:55:01 -0700

    Added solutions evaluation to Array solution #1

--- js/sum-mixed-array.js
+++ js/sum-mixed-array.js
@@ -4,3 +4,18 @@ function sumMix(x){
     return x.map(Number).reduce((a,c) => a+c); //.map() converts every item to a number (Number), and then .reduce() sums the values
 }
 
+//After reviewing solution there are several clever answers
+// I could have just done .reduce() with => acc + (+curr) 
+// or +curr + acc
+
+//or an eval( x.join("+")) which I think is particularly clever
+
+//or a for loop with parseInt() like this:
+
+// function sumMix(x){
+//     let result = 0;
+//     for (let n of x) {
+//     result += parseInt(n);
+//     }
+//     return result;
+// }
