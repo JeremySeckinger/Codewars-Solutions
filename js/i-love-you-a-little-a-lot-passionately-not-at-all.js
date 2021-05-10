a86d929b57cc57f6de171e728845e315c1280c3b
Jeremy Seckinger <jtseck@aol.com>
2021-05-09 18:56:39 -0700

    Added analysis after reviewing solutions for Array Solution #6

--- js/i-love-you-a-little-a-lot-passionately-not-at-all.js
+++ js/i-love-you-a-little-a-lot-passionately-not-at-all.js
@@ -14,3 +14,10 @@ function howMuchILoveYou(nbPetals) {
       return arr[5]
     }
 }
+
+//my first thought on this was to create an array of the defined returns, and use an algorithm to calculate the parameters return
+//applying a modulus for reach individual parameter controls for numbers past the 6 defined returns, but my method was brute force
+//this method above works, but it is NOT pretty and I knew there was an easier way to do this but my head doesnt want to think too deeply right now
+//analyzing other solutions looks like (n-1) % 6 was the solution I was looking for to tidy this up....so plainly obvious BIG OOF
+
+//I am glad I figured to make an array of the returns and use modulus to get the remainder to translate the return though--its something I guess
