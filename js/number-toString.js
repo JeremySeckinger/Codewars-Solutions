9d4633e603056afae7badba9c0274b9284cbea7e
Jeremy Seckinger <jtseck@aol.com>
2021-05-23 11:21:49 -0700

    Added string solution #4 evaluation

--- js/number-toString.js
+++ js/number-toString.js
@@ -1,3 +1,18 @@
 //fix this code a = 123.toString
 
 var a = '123'.toString();
+
+
+//there was no need for me to add the .toString() since I just added quotes to the 123 and that wouldve sufficed--stupid overlook
+
+
+//evaluating other solutions
+
+//var a = String(123);
+
+//var a = "123";
+
+// var a = 123..toString();
+// var a = 123 .toString()
+// var a = (123).toString();
+// var a = 123 + '';
