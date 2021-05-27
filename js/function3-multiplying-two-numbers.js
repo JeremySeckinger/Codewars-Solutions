cfc38329602baaf86c56cc43708809778a6544d7
Jeremy Seckinger <jtseck@aol.com>
2021-05-27 09:49:25 -0700

    Added solution with analysis of solutions

---
+++ js/function3-multiplying-two-numbers.js
@@ -0,0 +1,12 @@
+const multiply = (num1, num2) => {return num1 * num2};
+
+//could have made it simpler like this:
+
+//let multiply = (a, b) => a * b;
+
+//or added checks for num:
+
+// function multiply(a, b){
+//     if(typeof a == 'number' && typeof b == 'number')
+//       return a * b;  
+//   }
