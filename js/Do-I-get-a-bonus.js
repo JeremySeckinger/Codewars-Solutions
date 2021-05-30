98bb26748a292bfeb1e5df262434b75bf463bb2e
Jeremy Seckinger <jtseck@aol.com>
2021-05-30 09:39:12 -0700

    Added analysis for string solution

--- js/Do-I-get-a-bonus.js
+++ js/Do-I-get-a-bonus.js
@@ -6,3 +6,14 @@ function bonusTime(salary, bonus) {
      //return total as string prefixed with regex "\u00A3" for JS
      return bonus === true ? "\u00A3" + `${salary * 10}` : "\u00A3" + `${salary}`;
 }
+
+//after reviewing solutions I could have skipped the "bonues == true" since it is already a boolean and just left "bonus ?"
+//as well I could've used the £ sign in the template literal avoiding the regex
+
+//some interesting solves:
+
+// const bonusTime = (salary, bonus) => `£${salary * (bonus ? 10 : 1)}`;
+
+// function bonusTime(salary, bonus) {
+//     return '£' + salary * (bonus ? 10 : 1);
+// }
